349a54b591cab7ae74f5f7c8346f7844935edff7
yousefmohamed440 <yousefmahmed440@gmail.com>
2026-02-28 16:39:28 +0200

    Add files via upload

---
+++ D. Print Digits using Recursion.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+void printDigits(int n) {
+    if (n < 10) {
+        cout << n;
+        return;
+    }
+
+    printDigits(n / 10); //O(lon n)  -> base 10
+    cout << " " << n % 10; // O(1)
+}
+
+int main() {
+    int T;
+    cin >> T; // O(1)
+
+    vector<int> numbers(T);
+
+    for (int i = 0; i < T; i++) {  //O(n)
+        cin >> numbers[i];   // O(1)
+    }
+
+    for (int i = 0; i < T; i++) { //O(n)
+        if (numbers[i] == 0)
+            cout << 0;
+        else
+            printDigits(numbers[i]); //O(log n)
+
+        cout << endl;
+    }
+    // 2n + nlog(n) + log(n) + const => O(nlog n)  
+    return 0;
+}
